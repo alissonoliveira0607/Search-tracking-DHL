003a864432743f20d19a93568614d8b923f31784
Alisson Oliveira <alissonoliveira0607@gmail.com>
2023-11-29 16:18:08 -0300

    pull/1 - changed the main and made the PR adjustments

--- main_v2.py
+++ main_v2.py
@@ -1,20 +1,20 @@
 import os
 import requests
+import json
 import xml.etree.ElementTree as ET
 import pandas as pd
 from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
 from datetime import datetime
 from tkinter import Tk, filedialog
 from dotenv import load_dotenv
+import logging
 
-class DHLRequestBuilder:
-    def __init__(self, site_id, password, awb):
-        self.site_id = site_id
-        self.password = password
-        self.awb = awb
-        
-    def build_xml(self):
+# Configurando o sistema de logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class DHLRequestTracking:
+    def get_tracking_request(self, site_id, password, awb):
         xml_template = """<?xml version="1.0" encoding="UTF-8"?>
             <req:KnownTrackingRequest xmlns:req="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com TrackingRequestKnown.xsd" schemaVersion="1.0">
                 <Request>
@@ -27,41 +27,35 @@ class DHLRequestBuilder:
                 <AWBNumber>{}</AWBNumber>
                 <LevelOfDetails>ALL_CHECK_POINTS</LevelOfDetails>
             </req:KnownTrackingRequest>"""
-        return xml_template.format(self.site_id, self.password, self.awb)
+        return xml_template.format(site_id, password, awb)
 
 class DHLTracker:
-    def __init__(self, site_id, password):
-        self.site_id = site_id
-        self.password = password
-
-    def get_awb(self, awb, exists_history=False):
+    def get_awb(self, site_id, password, awb, exists_history=False):
         try:
             load_dotenv()
             url = os.getenv("URL")
-            headers_str = os.getenv("HEADERS")
+            headers_env = os.getenv("HEADERS")
 
             # Convertendo a string JSON em um dicionário
-            headers = eval(headers_str)
+            headers = json.loads(headers_env)
 
-            request_builder = DHLRequestBuilder(self.site_id, self.password, awb)
+            request_builder = DHLRequestTracking()
 
-            data = request_builder.build_xml()
+            data = request_builder.get_tracking_request(site_id, password, awb)
 
             response = requests.post(url, headers=headers, data=data)
-            response.raise_for_status()  # Adiciona esta linha para verificar se houve um erro HTTP
+            response.raise_for_status()
 
             if response.status_code == 200:
                 root = ET.fromstring(response.text)
                 events = self.extract_events(root)
-                events_sorted = sorted(events, key=lambda x: (x[0], x[1]))
+                events_sorted = sorted(events, key=lambda event: (event['date'], event['time']))
                 return events_sorted if exists_history else events_sorted[-1:]
             else:
-                print(f'A requisição para a AWB {awb} falhou com o código de status {response.status_code}')
-                print(f'Resposta XML:\n{response.text}')
-                return None
+                logger.error(f'A requisição para a AWB {awb} falhou com o código de status {response.status_code}')
+                logger.error(f'Resposta XML:\n{response.text}')
         except requests.exceptions.RequestException as e:
-            print(f'Erro na requisição para a AWB {awb}: {e}')
-            return None
+            logger.error(f'Erro na requisição para a AWB {awb}: {e}')
 
     def extract_events(self, root):
         events = []
@@ -70,43 +64,42 @@ class DHLTracker:
             time = event.find('Time').text
             event_code = event.find('ServiceEvent/EventCode').text
             description = event.find('ServiceEvent/Description').text
-            events.append([date, time, event_code, description])
-        return events        
+            events.append({'date': date, 'time': time, 'event_code': event_code, 'description': description})
+        return events
 
 class Tracking:
     def __init__(self):
         self.now = datetime.now()
         self.formatted_date = self.now.strftime("%Y-%m-%d-%H-%M")
-        
-    def get_tracking_and_generate_report(self, input_file, output_file, exists_history=False):
+
+    def generate_report(self, input_file, output_file, exists_history=False):
         try:
             site_id = input("Digite o Site id do cliente: ")
             password = input("Digite a senha: ")
 
-            tracker = DHLTracker(site_id, password)
+            tracker = DHLTracker()
 
             dataframe = pd.read_excel(input_file)
             workbook = Workbook()
             worksheet = workbook.active
 
             headers_file = ['AWB', 'Date', 'Time', 'Event Code', 'Description']
-            for column_number, headers_file in enumerate(headers_file, 1):
-                worksheet.cell(row=1, column=column_number, value=headers_file)
+            for column_number, header in enumerate(headers_file, 1):
+                worksheet.cell(row=1, column=column_number, value=header)
 
             for index, row in dataframe.iterrows():
                 awb = str(row["AWB"])
-                events_sorted = tracker.get_awb(awb, exists_history)
+                events_sorted = tracker.get_awb(site_id, password, awb, exists_history)
 
-                # Correção aqui: Alterando a condição para verificar se há eventos
                 if events_sorted:
                     if exists_history:
                         for event in events_sorted:
-                            worksheet.append([awb] + event)
+                            worksheet.append([awb, event['date'], event['time'], event['event_code'], event['description']])
                     else:
-                        worksheet.append([awb] + events_sorted[0] if events_sorted else [awb] + ['N/A', 'N/A', 'N/A', 'N/A'])
+                        worksheet.append([awb, events_sorted[0]['date'], events_sorted[0]['time'], events_sorted[0]['event_code'], events_sorted[0]['description']] if events_sorted else [awb, 'N/A', 'N/A', 'N/A', 'N/A'])
             workbook.save(output_file)
         except Exception as e:
-            print(f'Erro durante a execução: {e}')
+            logger.error(f'Erro durante a execução: {e}')
 
 if __name__ == "__main__":
     try:
@@ -123,6 +116,6 @@ if __name__ == "__main__":
 
         output_file = f'{app.formatted_date}_results{"_historico" if history else ""}.xlsx'
 
-        app.get_tracking_and_generate_report(input_file, output_file, history)
+        app.generate_report(input_file, output_file, history)
     except Exception as e:
-        print(f'Erro durante a execução do programa: {e}')
+        logger.error(f'Erro durante a execução do programa: {e}')
