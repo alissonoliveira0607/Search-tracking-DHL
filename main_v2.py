e66bc34d5cc732521ea23159ccee6d867971ce35
Alisson Oliveira <alissonoliveira0607@gmail.com>
2023-11-29 14:44:14 -0300

    Refactored the logic

--- main_v2.py
+++ main_v2.py
@@ -1,41 +1,69 @@
+import os
 import requests
 import xml.etree.ElementTree as ET
-from tabulate import tabulate
 import pandas as pd
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from datetime import datetime
 from tkinter import Tk, filedialog
+from dotenv import load_dotenv
 
-now = datetime.now()
-formatted_date = now.strftime("%Y-%m-%d-%H-%M")
+class DHLRequestBuilder:
+    def __init__(self, site_id, password, awb):
+        self.site_id = site_id
+        self.password = password
+        self.awb = awb
+        
+    def build_xml(self):
+        xml_template = """<?xml version="1.0" encoding="UTF-8"?>
+            <req:KnownTrackingRequest xmlns:req="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com TrackingRequestKnown.xsd" schemaVersion="1.0">
+                <Request>
+                    <ServiceHeader>
+                        <SiteID>{}</SiteID>
+                        <Password>{}</Password>
+                    </ServiceHeader>
+                </Request>
+                <LanguageCode>BR</LanguageCode>
+                <AWBNumber>{}</AWBNumber>
+                <LevelOfDetails>ALL_CHECK_POINTS</LevelOfDetails>
+            </req:KnownTrackingRequest>"""
+        return xml_template.format(self.site_id, self.password, self.awb)
 
-def consultar_awb(site_id, password, awb, historico=False):
-    url = 'https://xmlpi-ea.dhl.com/XMLShippingServlet'
-    headers = {'Content-Type': 'application/xml'}
+class DHLTracker:
+    def __init__(self, site_id, password):
+        self.site_id = site_id
+        self.password = password
 
-    data = f'''<?xml version="1.0" encoding="UTF-8"?>
-        <req:KnownTrackingRequest xmlns:req="http://www.dhl.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.dhl.com TrackingRequestKnown.xsd" schemaVersion="1.0">
-            <Request>
-                <ServiceHeader>
-                    <SiteID>{site_id}</SiteID>
-                    <Password>{password}</Password>
-                </ServiceHeader>
-            </Request>
-            <LanguageCode>BR</LanguageCode>
-            <AWBNumber>{awb}</AWBNumber>
-            <LevelOfDetails>ALL_CHECK_POINTS</LevelOfDetails>
-        </req:KnownTrackingRequest>'''
+    def get_awb(self, awb, exists_history=False):
+        try:
+            load_dotenv()
+            url = os.getenv("URL")
+            headers_str = os.getenv("HEADERS")
 
-    # Faz a requisição
-    response = requests.post(url, headers=headers, data=data)
+            # Convertendo a string JSON em um dicionário
+            headers = eval(headers_str)
 
-    # Verifica se a requisição foi bem-sucedida
-    if response.status_code == 200:
-        # Analisa a resposta XML
-        root = ET.fromstring(response.text)
+            request_builder = DHLRequestBuilder(self.site_id, self.password, awb)
 
-        # Extrai informações relevantes
+            data = request_builder.build_xml()
+
+            response = requests.post(url, headers=headers, data=data)
+            response.raise_for_status()  # Adiciona esta linha para verificar se houve um erro HTTP
+
+            if response.status_code == 200:
+                root = ET.fromstring(response.text)
+                events = self.extract_events(root)
+                events_sorted = sorted(events, key=lambda x: (x[0], x[1]))
+                return events_sorted if exists_history else events_sorted[-1:]
+            else:
+                print(f'A requisição para a AWB {awb} falhou com o código de status {response.status_code}')
+                print(f'Resposta XML:\n{response.text}')
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f'Erro na requisição para a AWB {awb}: {e}')
+            return None
+
+    def extract_events(self, root):
         events = []
         for event in root.findall('.//ShipmentEvent'):
             date = event.find('Date').text
@@ -43,67 +71,58 @@ def consultar_awb(site_id, password, awb, historico=False):
             event_code = event.find('ServiceEvent/EventCode').text
             description = event.find('ServiceEvent/Description').text
             events.append([date, time, event_code, description])
+        return events        
 
-        # Ordena os eventos por data e hora
-        events_sorted = sorted(events, key=lambda x: (x[0], x[1]))
+class Tracking:
+    def __init__(self):
+        self.now = datetime.now()
+        self.formatted_date = self.now.strftime("%Y-%m-%d-%H-%M")
+        
+    def get_tracking_and_generate_report(self, input_file, output_file, exists_history=False):
+        try:
+            site_id = input("Digite o Site id do cliente: ")
+            password = input("Digite a senha: ")
 
-        # Retorna todo o histórico ou apenas o último evento, conforme a opção
-        return events_sorted if historico else events_sorted[-1:]
+            tracker = DHLTracker(site_id, password)
 
-    else:
-        print(f'A requisição para a AWB {awb} falhou com o código de status {response.status_code}')
-        print(f'Resposta XML:\n{response.text}')  # Adiciona esta linha para imprimir a resposta XML em caso de erro
-        return None
+            dataframe = pd.read_excel(input_file)
+            workbook = Workbook()
+            worksheet = workbook.active
 
-def main(input_file, output_file, historico=False):
-    site_id = input("Digite o Site id do cliente: ")
-    password = input("Digite a senha: ")
+            headers_file = ['AWB', 'Date', 'Time', 'Event Code', 'Description']
+            for column_number, headers_file in enumerate(headers_file, 1):
+                worksheet.cell(row=1, column=column_number, value=headers_file)
 
-    # Carrega o arquivo de entrada
-    df = pd.read_excel(input_file)
+            for index, row in dataframe.iterrows():
+                awb = str(row["AWB"])
+                events_sorted = tracker.get_awb(awb, exists_history)
 
-    # Cria um novo arquivo Excel para armazenar os resultados
-    wb = Workbook()
-    ws = wb.active
-
-    # Adiciona cabeçalhos às colunas no novo arquivo Excel
-    headers = ['AWB', 'Date', 'Time', 'Event Code', 'Description']
-    for col_num, header in enumerate(headers, 1):
-        ws.cell(row=1, column=col_num, value=header)
-
-    # Itera sobre as linhas do DataFrame
-    for index, row in df.iterrows():
-        awb = str(row['AWB'])  # Converte para string, pois pode haver AWBs que são números inteiros
-        events_sorted = consultar_awb(site_id, password, awb, historico)
-
-        # Adiciona resultados ao novo arquivo Excel
-        if events_sorted:
-            if historico:
-                # Salva todo o histórico
-                for event in events_sorted:
-                    ws.append([awb] + event)
-            else:
-                # Salva apenas o último evento
-                ws.append([awb] + events_sorted[0] if events_sorted else [awb] + ['N/A', 'N/A', 'N/A', 'N/A'])
-
-    # Salva o novo arquivo Excel
-    wb.save(output_file)
+                # Correção aqui: Alterando a condição para verificar se há eventos
+                if events_sorted:
+                    if exists_history:
+                        for event in events_sorted:
+                            worksheet.append([awb] + event)
+                    else:
+                        worksheet.append([awb] + events_sorted[0] if events_sorted else [awb] + ['N/A', 'N/A', 'N/A', 'N/A'])
+            workbook.save(output_file)
+        except Exception as e:
+            print(f'Erro durante a execução: {e}')
 
 if __name__ == "__main__":
-    # Cria a janela de diálogo para seleção de arquivo
-    root = Tk()
-    root.withdraw()  # Oculta a janela principal
+    try:
+        root = Tk()
+        root.withdraw()
 
-    print("Selecione o arquivo de entrada")
+        print("Selecione o arquivo de entrada")
 
-    # Obtém o caminho do arquivo de entrada
-    input_file = filedialog.askopenfilename(title="Selecione o arquivo de entrada", filetypes=[("Arquivos Excel", "*.xlsx")])
+        input_file = filedialog.askopenfilename(title="Selecione o arquivo de entrada", filetypes=[("Arquivos Excel", "*.xlsx")])
 
-    # Define se deve salvar o histórico completo ou apenas o último evento
-    historico = input("Deseja obter o histórico completo? (S/N): ").upper() == 'S'
+        historico = input("Deseja obter o histórico completo? (S/N): ").upper() == 'S'
 
-    # Define o caminho do arquivo de saída
-    output_file = f'{formatted_date}_results{"_historico" if historico else ""}.xlsx'
+        app = Tracking()
 
-    # Executa a função principal
-    main(input_file, output_file, historico)
+        output_file = f'{app.formatted_date}_results{"_historico" if historico else ""}.xlsx'
+
+        app.get_tracking_and_generate_report(input_file, output_file, historico)
+    except Exception as e:
+        print(f'Erro durante a execução do programa: {e}')
